a96b8abc0d461b07accae407c8dfccd1c80eeb59
chutchev <chutchev>
2019-03-05 16:34:25 +0500

    Был произведен рефакторинг кода

--- duplicates.py
+++ duplicates.py
@@ -1,7 +1,8 @@
 import hashlib
 import os
-import sys
+import argparse
 import time
+
 
 def read_hash(filename):
     with open(filename, 'rb') as f:
@@ -11,65 +12,47 @@ def read_hash(filename):
         return hash_md5.hexdigest()
 
 
-def fill_list_duplicates(path):
-    dups = {}
-    index = 0
-    for dir, subdirs, files in os.walk(path):
+def fill_list_files_with_md5_hash(path):
+    md5_files_dict = {}
+    for dir, subdir, files in os.walk(path):
         for f in files:
             filename = os.path.join(dir, f)
-            dups[index] = {'md5': read_hash(filename), 'path': filename}
-            index += 1
+            hash = read_hash(filename)
+            md5_files_dict[filename] = {'md5': hash}
+    return md5_files_dict
+
+
+def find_duplicates(md5_files_dict):
+    dups = {}
+    for filename, hash in md5_files_dict.items():
+        dups_filename = []
+        for other_name, other_hash in md5_files_dict.items():
+            if hash['md5'] == other_hash['md5'] and filename != other_name:
+                dups_filename.append(other_name)
+            if len(dups_filename) > 0:
+                dups[hash['md5']] = {'original': filename, 'duplicates': dups_filename}
     return dups
 
 
-def find_duplicates(dups):
-    dupl = {}
-    for index in range(len(dups)):
-        for jindex in range(len(dups)):
-            if dups[index]['md5'] == dups[jindex]['md5'] and dups[index]['path'] != dups[jindex]['path']:
-                dupl[dups[index]['path']] = {'duplicates': dups[jindex]['path'], 'md5': dups[jindex]['md5']}
-    for filename in dupl.keys():
-        with open(os.path.abspath("duplicates.txt"), "a") as dublicates_txt:
-            dublicates_txt.write(f"{filename}: \n\t{dupl[filename]['duplicates']}  {dupl[filename]['md5']}\n")
-    return dupl
-
-
-def fill_delete_list(dupl):
-    delete_list = dupl.copy()
-    keys_list = []
-    for index in dupl.keys():
-        keys_list.append(index)
-    for duplicate_name in dupl.keys():
-        keys_list.pop(keys_list.index(duplicate_name))
-        for filename in keys_list:
-            if duplicate_name != filename:
-                if dupl[duplicate_name]['duplicates'] == filename:
-                    delete_list[filename] = {'duplicates': 'File must be deleted!!!'}
-    for filename in delete_list.keys():
-        with open(os.path.abspath("for_duplicates_delete.txt"), "a") as dublicates_txt:
-            dublicates_txt.write(f"{filename}:\n\t{delete_list[filename]['duplicates']}\n")
-    return delete_list
-
-
-def delete_duplicates(duplicates):
-    for filenames in duplicates.keys():
-        if duplicates[filenames]['duplicates'] != "File must be deleted!!!":
-            with open(os.path.abspath("delete_duplicates.txt"), "a") as dublicates_txt:
-                dublicates_txt.write(f"{filenames}\n")
-            os.remove(filenames)
+def delete_duplicates(dups):
+    for value in dups.values():
+        for f in value['duplicates']:
+            os.remove(f)
+            print(f'Файл: {f} был удален')
 
 
 def main():
-    path = sys.argv[1]
-    start_time = time.time()
-    dubl = find_duplicates(fill_list_duplicates(path))
-    delete_file = fill_delete_list(dubl)
-    delete = sys.argv[2]
-    if delete.lower() == 'y':
-        delete_duplicates(delete_file)
-    elif delete.lower() == 'n':
-        pass
-    print(f"Сделано за {time.time()-start_time}")
+    parser= argparse.ArgumentParser(description="USAGE: python duplicates.py path_to_directory_with_duplicates y/n")
+    parser.add_argument('path', help="Переменная для установки пути для поиска дубликатов")
+    parser.add_argument('delete', help="Флаг для разрешения/отказа на удаление дубликатов")
+    args = parser.parse_args()
+    md5_file_dict = fill_list_files_with_md5_hash(args.path)
+    dict_dups = find_duplicates(md5_file_dict)
+    if args.delete == 'y':
+        delete_duplicates(dict_dups)
+        print('Поиск дубликатов выполнен. Файлы были удалены, так как было дано ваше разрешение.')
+    else:
+        print('Поиск дубликатов выполнен. Файлы не были удалены, так как не было дано ваше разрешение.')
 
 
 if __name__ == '__main__':
